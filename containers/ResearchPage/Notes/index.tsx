31714d2dd555ecbf87cb31a5c7ac6e0020016708
okmttdhr <10101114fin@gmail.com>
2020-04-18 22:46:24 +0900

    feat: Linkify Notes text

--- containers/ResearchPage/Notes/index.tsx
+++ containers/ResearchPage/Notes/index.tsx
@@ -1,26 +1,20 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 import { useNotes } from '../hooks/useNotes';
+import ContentEditable from 'react-contenteditable';
 
 type Props = ReturnType<typeof useNotes>;
 
 export const Notes: React.FC<Props> = ({ notes, onChange }) => {
   return (
     <>
-      <Textarea value={notes} onChange={onChange} />
-      <ContentEditable contentEditable></ContentEditable>
+      <StyledContentEditable html={notes} onChange={onChange}></StyledContentEditable>
     </>
   );
 };
 
-const Textarea = styled.textarea`
-  width: 100%;
-  min-height: 100px;
-  resize: vertical;
+const StyledContentEditable = styled(ContentEditable)`
   padding: 5px;
-  box-sizing: border-box;
-`;
-
-const ContentEditable = styled.div`
+  min-height: 100px;
   border: 1px solid #ddd;
 `;
--- containers/ResearchPage/hooks/useNotes.ts
+++ containers/ResearchPage/hooks/useNotes.ts
@@ -1,5 +1,6 @@
 import { useCallback, Dispatch, SetStateAction, useMemo } from 'react';
 import { Research, Researches } from 'types/research';
+import linkifyHtml from 'linkifyjs/html';
 
 export const useNotes = (research: Research, setResearches: Dispatch<SetStateAction<Researches>>) => {
   const notes = useMemo(() => {
@@ -9,12 +10,16 @@ export const useNotes = (research: Research, setResearches: Dispatch<SetStateAct
   const onChange = useCallback(
     (event: React.SyntheticEvent) => {
       const target = event.target as HTMLInputElement;
+
+      // TODO: Consider performance
+      const notes = linkifyHtml(target.value, { attributes: { contenteditable: 'false' } });
+
       setResearches((rs) => {
         return {
           ...rs,
           [research.id]: {
             ...research,
-            notes: target.value,
+            notes,
           },
         };
       });
